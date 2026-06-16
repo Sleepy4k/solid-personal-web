08737f7e74f20bc02ddbb8d87d48e811aa790194
Apri Pandu W <sarahpalastrin@gmail.com>
2026-06-17 01:44:48 +0700

    chore(config): update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,21 @@ import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [
-    solidStart({
-      middleware: "./src/middleware/index.ts"
-    }),
-    tailwindcss(),
-    nitro({
-      routeRules: {
-        "/": { headers: { "Cache-Control": "public, max-age=300, stale-while-revalidate=3600" } },
-        "/dashboard/**": { headers: { "Cache-Control": "private, no-store" } },
-        "/uploads/**": { headers: { "Cache-Control": "public, max-age=31536000, immutable" } }
-      }
-    })
-  ]
+export default defineConfig(({ command }) => {
+  return {
+    define: command === "build" ? { "import.meta.env.DEV": "false" } : {},
+    plugins: [
+      solidStart({
+        middleware: "./src/middleware/index.ts"
+      }),
+      tailwindcss(),
+      nitro({
+        routeRules: {
+          "/": { headers: { "Cache-Control": "public, max-age=300, stale-while-revalidate=3600" } },
+          "/dashboard/**": { headers: { "Cache-Control": "private, no-store" } },
+          "/uploads/**": { headers: { "Cache-Control": "public, max-age=31536000, immutable" } }
+        }
+      })
+    ]
+  };
 });
